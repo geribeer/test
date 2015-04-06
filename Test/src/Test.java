5441893f724cc1a0f3e467a368c1bc1ad1687b2f
Geraldine Beer <Gerry.beery@gmail.com>
2015-04-06 20:05:33 +0200

    test1
    Change-Id: Iaa0fa3bcfa855d09710b0b949b3bae9e3ff09785
    
    
    Change-Id: Iaa0fa3bcfa855d09710b0b949b3bae9e3ff09785
    Signed-off-by: Geraldine Beer <Gerry.beery@gmail.com>

---
+++ Test/src/Test.java
@@ -0,0 +1,19 @@
+public class Test {
+
+	/**
+	 * @author D060425
+	 * 
+	 */
+	public static void main(String[] args) {
+		printHello();
+	}
+
+	private static void printHello() {
+		System.out.println("Hallo Codereview");
+	}
+
+	private static void printBye() {
+		System.out.println("Bye, bye Codereview");
+	}
+
+}
